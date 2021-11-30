d2842a253d9e16019361ce7b5e56883390214f27
Git <metalfacesec@gmail.com>
2021-11-30 18:32:25 -0500

    Changing export structure

--- src/index.js
+++ src/index.js
@@ -2,10 +2,4 @@ const Wallet = require('./models/wallet/Wallet');
 
 process.env['NODE_TLS_REJECT_UNAUTHORIZED'] = 0;
 
-class Chia {
-	static wallet() {
-		return new Wallet();
-	}
-}
-
-module.exports = Chia;
+module.exports = { Wallet };
