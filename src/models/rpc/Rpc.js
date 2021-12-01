2782fe56e970f0ffaf2c99bc6b8656abcfb969a6
Git <metalfacesec@gmail.com>
2021-11-30 21:54:05 -0500

    Adding passing config object into constructors

--- src/models/rpc/Rpc.js
+++ src/models/rpc/Rpc.js
@@ -2,15 +2,45 @@ const fs = require('fs');
 const https = require('https');
 
 class Rpc {
-	makePostRequest(endpoint, data) {
+	constructor(config) {
+		if (config === null || typeof config !== 'object') {
+			return console.log('ERROR: Invalid config passed into constructor!');
+		}
+
+		if (typeof config.rootKeyDir !== 'string' || !config.rootKeyDir.length) {
+			return console.log('ERROR: Invalid rootKeyDir passed into constructor!');
+		}
+		this.rootKeyDir = config.rootKeyDir;
+		
+		this.hostname = '127.0.0.1';
+		if (typeof config.hostname === 'string' && config.hostname.length) {
+			this.hostname = config.hostname;
+		}
+
+		this.walletPort = 9256;
+		if (typeof config.walletPort !== 'undefined' && Number.isInteger(config.walletPort)) {
+			this.walletPort = config.walletPort;
+		}
+	}
+	
+	makePostRequest(endpoint, data, keyNamespace) {
 		return new Promise((resolve, reject) => {
+			let port = null;
+			if (keyNamespace === 'wallet') {
+				port = this.walletPort;
+			}
+
+			if (port === null) {
+				return console.log('ERROR: Rpc.makePostRequest - Could not match port to namespace!');
+			}
+
 			let postData = JSON.stringify(data);
 	
 			let options = {
-				key: fs.readFileSync(''),
-				cert: fs.readFileSync(''),
-				hostname: '127.0.0.1',
-				port: 9256,
+				key: fs.readFileSync(`${this.rootKeyDir}/${keyNamespace}/private_wallet.key`),
+				cert: fs.readFileSync(`${this.rootKeyDir}/${keyNamespace}/private_wallet.crt`),
+				hostname: this.hostname,
+				port: port,
 				path: endpoint,
 				method: 'POST',
 				headers: {
