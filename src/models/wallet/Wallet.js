053ee8459fe79d2efd6e9cafb368d1dffce99933
Git <metalfacesec@gmail.com>
2021-11-30 21:54:28 -0500

    Adding more wallet endpoints

--- src/models/wallet/Wallet.js
+++ src/models/wallet/Wallet.js
@@ -1,22 +1,54 @@
 const Rpc = require('../rpc/Rpc');
 
 class Wallet extends Rpc {
-	constructor() {
-		super();
+	constructor(config) {
+		super(config);
 
 		this.wallets = [];
 	}
 
 	async getWallets() {
-		return await this.makePostRequest('/get_wallets', {});
+		return await this.makePostRequest('/get_wallets', {}, 'wallet');
 	}
 
 	async getPublicKeys() {
-		return await this.makePostRequest('/get_public_keys', {});
+		return await this.makePostRequest('/get_public_keys', {}, 'wallet');
 	}
 
 	async getPrivateKey(fingerprint) {
-		return await this.makePostRequest('/get_private_key', {fingerprint: fingerprint});
+		return await this.makePostRequest('/get_private_key', {fingerprint: fingerprint}, 'wallet');
+	}
+
+	async getSyncStatus() {
+		return await this.makePostRequest('/get_sync_status', {}, 'wallet');
+	}
+
+	async getHeightInfo() {
+		return await this.makePostRequest('/get_height_info', {}, 'wallet');
+	}
+
+	async getWalletBalance(walletId) {
+		return await this.makePostRequest('/get_wallet_balance', {wallet_id: walletId}, 'wallet');
+	}
+
+	async getFarmedAmount() {
+		return await this.makePostRequest('/get_farmed_amount', {}, 'wallet');
+	}
+
+	async generateMnemonic() {
+		return await this.makePostRequest('/generate_mnemonic', {}, 'wallet');
+	}
+
+	async getNetworkInfo() {
+		return await this.makePostRequest('/get_network_info', {}, 'wallet');
+	}
+
+	async getTransactions(walletId) {
+		return await this.makePostRequest('/get_transactions', {wallet_id: walletId}, 'wallet');
+	}
+
+	async getTransactionCount(walletId) {
+		return await this.makePostRequest('/get_transaction_count', {wallet_id: walletId}, 'wallet');
 	}
 }
 
