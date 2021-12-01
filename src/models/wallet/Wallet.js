9ab4b91cd7f3f263329d7102f7c4694ae83a7a1f
Git <metalfacesec@gmail.com>
2021-11-30 23:19:03 -0500

    Moving file name and path to variable

--- src/models/wallet/Wallet.js
+++ src/models/wallet/Wallet.js
@@ -4,55 +4,55 @@ class Wallet extends Rpc {
 	constructor(config) {
 		super(config);
 
-		this.wallets = [];
+		this.namespace = 'wallet';
 	}
 
 	async getWallets() {
-		return await this.makePostRequest('/get_wallets', {}, 'wallet');
+		return await this.makePostRequest('/get_wallets', {}, this.namespace);
 	}
 
 	async getPublicKeys() {
-		return await this.makePostRequest('/get_public_keys', {}, 'wallet');
+		return await this.makePostRequest('/get_public_keys', {}, this.namespace);
 	}
 
 	async getPrivateKey(fingerprint) {
-		return await this.makePostRequest('/get_private_key', {fingerprint: fingerprint}, 'wallet');
+		return await this.makePostRequest('/get_private_key', {fingerprint: fingerprint}, this.namespace);
 	}
 
 	async getSyncStatus() {
-		return await this.makePostRequest('/get_sync_status', {}, 'wallet');
+		return await this.makePostRequest('/get_sync_status', {}, this.namespace);
 	}
 
 	async getHeightInfo() {
-		return await this.makePostRequest('/get_height_info', {}, 'wallet');
+		return await this.makePostRequest('/get_height_info', {}, this.namespace);
 	}
 
 	async getWalletBalance(walletId) {
-		return await this.makePostRequest('/get_wallet_balance', {wallet_id: walletId}, 'wallet');
+		return await this.makePostRequest('/get_wallet_balance', {wallet_id: walletId}, this.namespace);
 	}
 
 	async getFarmedAmount() {
-		return await this.makePostRequest('/get_farmed_amount', {}, 'wallet');
+		return await this.makePostRequest('/get_farmed_amount', {}, this.namespace);
 	}
 
 	async generateMnemonic() {
-		return await this.makePostRequest('/generate_mnemonic', {}, 'wallet');
+		return await this.makePostRequest('/generate_mnemonic', {}, this.namespace);
 	}
 
 	async getNetworkInfo() {
-		return await this.makePostRequest('/get_network_info', {}, 'wallet');
+		return await this.makePostRequest('/get_network_info', {}, this.namespace);
 	}
 
 	async getTransactions(walletId) {
-		return await this.makePostRequest('/get_transactions', {wallet_id: walletId}, 'wallet');
+		return await this.makePostRequest('/get_transactions', {wallet_id: walletId}, this.namespace);
 	}
 
 	async getTransactionCount(walletId) {
-		return await this.makePostRequest('/get_transaction_count', {wallet_id: walletId}, 'wallet');
+		return await this.makePostRequest('/get_transaction_count', {wallet_id: walletId}, this.namespace);
 	}
 
 	async addKey(mnemonic) {
-		return await this.makePostRequest('/add_key', {mnemonic: mnemonic, type: 'new_wallet'}, 'wallet');
+		return await this.makePostRequest('/add_key', {mnemonic: mnemonic, type: 'new_wallet'}, this.namespace);
 	}
 }
 
